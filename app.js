170f6bba8eef2d23d137adeda4e8257543840aec
rodel-lab <62780594+rodel-lab@users.noreply.github.com>
2020-11-22 14:21:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,27 @@ const phrases = [
 ];
 
 // Attach an eventlistener to the 'Start Game' buton to hid the start screen overlay
-btnreset.addEventListener('click', () => {
-    overlay.style.display = 'none';
+// listen for the start game button to be pressed
+btnreset.addEventListener('click', () => { //add eventlistener to a variable btn_reset
+    overlay.style.display = 'none'; //hide the overlay by changing its display property
 
 });
+
+// Create a getRandomPhraseAsArray function
+const getrandomPhraseAsArray = arr => { // create a function 'stub' taken from the Examples in pdf file
+const randomNum = Math.floor(Math.random() * phrases.length) //create a variable to store a random number
+    return getrandomPhraseAsArray(randomNum); //return the array element at that index
+}
+getRandomPhraseAsArray(phrases) // After creating the getRandomPhraseAsArray, need to "call" it
+
+// Check if a letter is in the phrase
+const checkLetter = button => {
+    const li = document.getElementsByClassName('li')
+    const matchFound = null;
+    for (let i = 0; i < li.length; i++){
+
+    }
+    if () {
+       
+    }
+}
